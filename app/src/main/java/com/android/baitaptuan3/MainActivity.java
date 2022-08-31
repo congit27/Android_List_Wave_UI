5751a1fb37f5c48b37fd106f6256f33a8479847e
Cong <conbo1102a@gmail.com>
2022-08-31 09:52:41 +0700

    update project

--- app/src/main/java/com/android/baitaptuan3/MainActivity.java
+++ app/src/main/java/com/android/baitaptuan3/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_ui);
+        setContentView(R.layout.wave_ui);
     }
 }
